9a698ae49de7e2ffb13e4657cb7104dbfda7b5ff
Sukant Kar <69027716+sukant-kar@users.noreply.github.com>
2022-08-22 12:27:28 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+BITCOIN_PRICE_THRESHOLD = 10000  # Set this to whatever you like
+
+def main():
+    bitcoin_history = []
+    while True:
+        price = get_latest_bitcoin_price()
+        date = datetime.now()
+        bitcoin_history.append({'date': date, 'price': price})
+
+        # Send an emergency notification
+        if price < BITCOIN_PRICE_THRESHOLD:
+            post_ifttt_webhook('bitcoin_price_emergency', price)
+
+        # Send a Telegram notification
+        # Once we have 5 items in our bitcoin_history send an update
+        if len(bitcoin_history) == 5:
+            post_ifttt_webhook('bitcoin_price_update', 
+                               format_bitcoin_history(bitcoin_history))
+            # Reset the history
+            bitcoin_history = []
+
+        # Sleep for 5 minutes 
+        # (For testing purposes you can set it to a lower number)
+        time.sleep(5 * 60)
